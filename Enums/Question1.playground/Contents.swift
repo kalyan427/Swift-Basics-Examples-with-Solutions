aa0aa100f3ebe48f8d12c9afe64bfd94685ee8d9
Kalyan <pvkalyan427@gmail.com>
2023-08-26 21:21:22 -0400

    Define an enumeration called Season that represents the four seasons of the year

---
+++ Enums/Question1.playground/Contents.swift
@@ -0,0 +1,21 @@
+
+/* Define an enumeration called "Season" that represents the four seasons of the year */
+
+import Cocoa
+
+enum Season {
+    case summer
+    case winter
+    case fall
+    case spring
+}
+    
+
+
+
+
+
+
+
+
+
