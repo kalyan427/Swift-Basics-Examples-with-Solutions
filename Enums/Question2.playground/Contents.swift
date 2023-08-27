30ca59493c47ec4106265807b8391275ed4f1d3b
Kalyan <pvkalyan427@gmail.com>
2023-08-26 21:34:52 -0400

    Second Question

---
+++ Enums/Question2.playground/Contents.swift
@@ -0,0 +1,22 @@
+
+/* Create an enumeration named "MathOperation" with cases for addition, subtraction, multiplication, and division. Include associated values to hold the operands */
+
+import Cocoa
+
+enum MathOperation {
+    case addition(Int, Int)
+    case subtraction(Int, Int)
+    case multiplication(Int, Int)
+    case division(Int, Int)
+}
+
+    
+
+
+
+
+
+
+
+
+
