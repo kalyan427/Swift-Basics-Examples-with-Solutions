afe39588c7e4ed1af32fd0c0ceb7b6f6c584ecb0
Kalyan <pvkalyan427@gmail.com>
2023-08-26 21:39:29 -0400

    Third Question

---
+++ Enums/Question3.playground/Contents.swift
@@ -0,0 +1,20 @@
+
+/* Define an enumeration named "CoffeeSize" that has three sizes: small, medium, and large. Use string raw values to represent each size */
+
+import Cocoa
+
+enum CoffeeSize {
+    case small = "Small"
+    case medium = "Medium"
+    case large = "Large"
+}
+    
+
+
+
+
+
+
+
+
+
