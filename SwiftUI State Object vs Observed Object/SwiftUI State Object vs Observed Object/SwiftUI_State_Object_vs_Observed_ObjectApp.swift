07829693ab4e6cdc5cf0564fdc1caef508cc588a
Kalyan <pvkalyan427@gmail.com>
2024-01-10 20:58:09 -0500

    Initial Commit

---
+++ SwiftUI State Object vs Observed Object/SwiftUI State Object vs Observed Object/SwiftUI_State_Object_vs_Observed_ObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_State_Object_vs_Observed_ObjectApp.swift
+//  SwiftUI State Object vs Observed Object
+//
+//  Created by kalyan . on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_State_Object_vs_Observed_ObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
