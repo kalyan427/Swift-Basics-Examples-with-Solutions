8961b02efa776396475ca8cdc7411eadd1b57bcb
Kalyan <pvkalyan427@gmail.com>
2024-01-13 13:16:24 -0500

    Request/Post API call

--- POST API /User Registration POST API Call/User Registration POST API Call/ViewController.swift
+++ POST API /User Registration POST API Call/User Registration POST API Call/ViewController.swift
@@ -12,8 +12,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        makePOSTAPICall()
     }
-
-
+    
+    func makePOSTAPICall() {
+        guard let url = URL(string: "https://httpbin.org/post") else {
+            print("Error")
+            return
+        }
+        
+        let userData: [String: Any] = [
+            "username": "admin",
+            "password": "password123"
+        ]
+        
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: userData) else {
+            print("Error")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error")
+            }
+            
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("\(dataString)")
+            }
+        }.resume()
+    }
 }
 
