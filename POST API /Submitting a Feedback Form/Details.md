a01b9b4475690fb2d51491feb2785118e8c7eb2b
Kalyan <pvkalyan427@gmail.com>
2024-01-14 01:28:54 -0500

    Solution + Added Readme

---
+++ POST API /Submitting a Feedback Form/Details.md
@@ -0,0 +1,9 @@
+### Question
+
+#####  Submitting a Feedback Form
+ - Craft a POST request to submit a feedback form in application/x-www-form-urlencoded format.
+
+    - URL: https://httpbin.org/post
+    - Data: name=John+Doe&email=john@example.com&message=Great+Service
+
+
--- POST API /Submitting a Feedback Form/Submitting a Feedback Form/ViewController.swift
+++ POST API /Submitting a Feedback Form/Submitting a Feedback Form/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        makePOSTAPICall()
     }
     
     func makePOSTAPICall() {
@@ -19,8 +20,27 @@ class ViewController: UIViewController {
             print("Error")
             return
         }
+        
+        let urlEncodedString = "name=John+Doe&email=john@example.com&message=Great+Service"
+        guard let urlEncodedData = urlEncodedString.data(using: .utf8) else {
+            print("Error")
+            return
+        }
+                
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpBody = urlEncodedData
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print(error)
+            }
+            
+            if let data = data, let dataString = String(data: urlEncodedData, encoding: .utf8) {
+                print(dataString)
+            }
+        }.resume()
     }
-    
-    
 }
 
