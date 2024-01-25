21e34c04681237feb44f01ac3c9b33684b7e6661
Kalyan <pvkalyan427@gmail.com>
2024-01-24 23:00:36 -0500

    Replaced @State object with the @ObservedObject to get the latest changes.

--- SwiftUI State Object vs Observed Object/SwiftUI State Object vs Observed Object/ContentView.swift
+++ SwiftUI State Object vs Observed Object/SwiftUI State Object vs Observed Object/ContentView.swift
@@ -7,15 +7,42 @@
 
 import SwiftUI
 
+class ViewModel: ObservableObject {
+    @Published var heroNames: [String] = []
+    
+    init() {
+        defaultHeroNames()
+    }
+    
+    func defaultHeroNames() {
+        heroNames.append("Thor")
+        heroNames.append("IronMan")
+    }
+    
+    func addNewHeroName() {
+        heroNames.append("Captain America")
+        print(heroNames)
+    }
+}
+
 struct ContentView: View {
+    
+    @ObservedObject var viewModel: ViewModel = ViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(viewModel.heroNames, id: \.self) { heroName in
+                Text(heroName)
+            }
+            .navigationTitle("Marvel Heroes")
+            .toolbar(content: {
+                Button {
+                    viewModel.addNewHeroName()
+                } label: {
+                    Image(systemName: "plus")
+                }
+            })
         }
-        .padding()
     }
 }
 
