0db26dfbe13c6fa5b0edd6238253322bdeaac9af
Kalyan <pvkalyan427@gmail.com>
2024-09-22 18:33:03 -0400

    Using Minutes

---
+++ Control Flow/For-In Loop/Example 5/Using Ranges/Using Ranges/main.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  Using Ranges
+//
+//  Created by kalyan . on 9/22/24.
+//
+
+import Foundation
+
+let minutes = 60
+
+for minute in 0..<minutes {
+    print("Minutes \(minute)")
+}
+
+/*
+ Output:
+ 
+ Minutes 0
+ Minutes 1
+ Minutes 2
+ Minutes 3
+ Minutes 4
+ Minutes 5
+ Minutes 6
+ Minutes 7
+ Minutes 8
+ Minutes 9
+ Minutes 10
+ */
+
