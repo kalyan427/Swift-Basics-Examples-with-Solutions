f6a159db2893cb8f8aa7bc226facabe50c470c1e
Kalyan <pvkalyan427@gmail.com>
2024-09-22 18:04:27 -0400

    Using Numeric Ranges

---
+++ Control Flow/For-In Loop/Example 3/Using Numeric Ranges/Using Numeric Ranges/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  Using Numeric Ranges
+//
+//  Created by kalyan . on 9/22/24.
+//
+
+import Foundation
+
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5)")
+}
+
+/*
+ Output:
+ 
+ 1 times 5 is 5
+ 2 times 5 is 10
+ 3 times 5 is 15
+ 4 times 5 is 20
+ 5 times 5 is 25
+ */
