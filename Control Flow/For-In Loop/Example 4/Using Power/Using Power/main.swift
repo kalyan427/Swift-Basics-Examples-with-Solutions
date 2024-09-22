f4f526a6a0d6bb21d929aa4d96c7c58bbcc7f7c1
Kalyan <pvkalyan427@gmail.com>
2024-09-22 18:23:49 -0400

    Using Power

---
+++ Control Flow/For-In Loop/Example 4/Using Power/Using Power/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Using Power
+//
+//  Created by kalyan . on 9/22/24.
+//
+
+import Foundation
+
+let base = 4
+let power = 5
+var answer = 1
+
+for _ in 1...power {
+    answer *= power
+}
+
+print("\(base) to the power of \(power) is \(answer)")
+
